5e9d58299bae82f7f3335aa379d6e8ecd8046553
Ruben Gilbert <me@rubengilbert.com>
2023-08-30 18:59:03 -0700

    Completed implementation of initial Repos

--- api/content_manager/repositories.py
+++ api/content_manager/repositories.py
@@ -1,7 +1,8 @@
+from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Generic, List, Protocol, Type, TypeVar
 
-from sqlalchemy import select
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from content_manager.entities import ContentEntity, E
@@ -25,43 +26,38 @@ class Repository(Protocol[E]):
         ...
 
 
-# TODO: Finish making this class compatible with Repository Protocol (missing methods)
-class BaseRepository(Generic[E]):
+class BaseRepository(ABC, Generic[E]):
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
         self._entity_type = None
 
     @property
     def entity_type(self) -> Type[E]:
-        """
-        The derived entity type of this repository.
-        """
         if not self._entity_type:
             self._entity_type = get_derived_type(self)
         return self._entity_type
 
     async def add(self, entity: E) -> E:
-        """
-        TODO
-        """
         async with self.session.begin():
             self.session.add(entity)
         return entity
 
     async def get(self) -> List[E]:
-        """
-        TODO
-        """
         async with self.session.begin():
             result = await self.session.execute(select(self.entity_type))
             return [r[0] for r in result]
 
     async def get_by_id(self, id: int) -> E | None:
-        """
-        TODO
-        """
         async with self.session.begin():
             return await self.session.get(self.entity_type, id)
+
+    @abstractmethod
+    async def update(self, entity: E) -> E:
+        ...
+
+    @abstractmethod
+    async def remove(self, id: int) -> None:
+        ...
 
 
 R = TypeVar("R", bound=BaseRepository)
@@ -71,3 +67,14 @@ class ContentRepository(BaseRepository[ContentEntity]):
     async def add(self, entity: ContentEntity) -> ContentEntity:
         entity.last_renewed = datetime.now()
         return await super().add(entity)
+
+    async def update(self, entity: ContentEntity) -> ContentEntity:
+        async with self.session.begin():
+            existing = await self.session.get(self.entity_type, entity.id)
+            if not existing:
+                raise ValueError(f"Content with ID {entity.id} does not exist to be updated.")
+            return await self.session.merge(entity)
+
+    async def remove(self, id: int) -> None:
+        async with self.session.begin():
+            await self.session.execute(delete(self.entity_type).where(self.entity_type.id == id))
