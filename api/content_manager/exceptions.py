482179d54971539383c8994bb0090157f6a829d1
Ruben Gilbert <me@rubengilbert.com>
2023-09-01 18:41:18 -0700

    Adding exception for HTTP 409 Conflict.

--- api/content_manager/exceptions.py
+++ api/content_manager/exceptions.py
@@ -6,6 +6,11 @@ from fastapi.responses import JSONResponse
 from content_manager.models import Error
 
 
+class ConflictException(HTTPException):
+    def __init__(self, detail: Any = None, headers: dict[str, str] | None = None):
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail, headers=headers)
+
+
 class NotFoundException(HTTPException):
     def __init__(self, detail: Any = None, headers: dict[str, str] | None = None):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail, headers=headers)
